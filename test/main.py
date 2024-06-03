b60626b24c81d175a9b67688e9c6549dabde24d9
XiaoLuo <3428237080@qq.com>
2024-06-03 20:01:22 +0800

    第二次重构

--- test/main.py
+++ test/main.py
@@ -1,7 +1,14 @@
+import os.path
+
 import pandas as pd
 
 summary_files = []  # 存储各班汇总表文件夹中的表格路径
-summary_data = pd.DataFrame()  # 存储从各班汇总表中读取的数据，避免保存学生身份证号等敏感信息
+summary_data = pd.DataFrame(columns=['学号', '班级', '姓名'])  # 存储从各班汇总表中读取的数据，避免保存学生身份证号等敏感信息
+
 
 def open_summary_files():
+    folder_summary = os.path.abspath('../data/2024年春校内奖学金/各班汇总表')
+    if not folder_summary:
+        print('为选择文件')
+        return
 
